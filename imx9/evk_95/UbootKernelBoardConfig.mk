d8635fbd7f0a953af2ca39619b8dc5ee8b83a31f
Jiri Rezler <jiri.rezler@nxp.com>
2024-09-04 16:06:51 +0200

    AAUTO-1545 Add "secure_unlock" defconfig for imx95_19x19 EVK, Car & Car2 image types.
    Change-Id: I51f48ce9f22def773c99d9e5614cb0f5fe89d882
    Reviewed-on: http://androidsource.nxp.com/project/25837
    Reviewed-by: Petr Ondracek <petr.ondracek@nxp.com>

--- imx9/evk_95/UbootKernelBoardConfig.mk
+++ imx9/evk_95/UbootKernelBoardConfig.mk
@@ -5,8 +5,10 @@ UBOOT_POST_PROCESS := true
 ifeq ($(PRODUCT_IMX_CAR),true)
   ifeq ($(PRODUCT_IMX_CAR_M7),true)
     TARGET_BOOTLOADER_CONFIG := imx95:imx95_19x19_evk_androidauto_trusty_defconfig
+    TARGET_BOOTLOADER_CONFIG += imx95-secure-unlock:imx95_19x19_evk_androidauto_trusty_secure_unlock_defconfig
   else
     TARGET_BOOTLOADER_CONFIG := imx95:imx95_19x19_evk_androidauto2_trusty_defconfig
+    TARGET_BOOTLOADER_CONFIG += imx95-secure-unlock:imx95_19x19_evk_androidauto2_trusty_secure_unlock_defconfig
     TARGET_BOOTLOADER_CONFIG += imx95-verdin:imx95_19x19_verdin_androidauto2_trusty_defconfig
   endif #PRODUCT_IMX_CAR_M7
 else
