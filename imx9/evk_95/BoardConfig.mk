472a31fa70be3cd659a001e739b54ef8b633834d
Jiri Rezler <jiri.rezler@nxp.com>
2025-08-08 12:10:20 +0200

    AAUTO-2134 Use DPU composition for imx95 Car image type.
    Change-Id: Idd9142f717459af7dade8fdeaf2c3f50181fbde3
    Reviewed-on: http://androidsource.nxp.com/project/29486
    Reviewed-by: Petr Ondracek <petr.ondracek@nxp.com>

--- imx9/evk_95/BoardConfig.mk
+++ imx9/evk_95/BoardConfig.mk
@@ -170,11 +170,7 @@ BOARD_KERNEL_CMDLINE += cma=$(CMASIZE)@0xBF0M-0xFF0M
 
 # display config
 BOARD_BOOTCONFIG += androidboot.lcd_density=200
-ifeq ($(PRODUCT_IMX_CAR_M7),true)
-BOARD_BOOTCONFIG += androidboot.dpu_composition=0
-else
 BOARD_BOOTCONFIG += androidboot.dpu_composition=1
-endif
 
 # wifi config
 BOARD_BOOTCONFIG += androidboot.wificountrycode=US
