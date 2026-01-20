8867fd1dcc65336f53ea31fed026ee6a8ca5bb1c
Jindong Yue <jindong.yue@nxp.com>
2026-01-20 17:26:49 +0900

    MA-24439 audio: Add imx95 sof uboot targets
    The SM config is mx95evksof.cfg
    The mkimage option is flash_a55
    
    Change-Id: I8ebf5919f36df22e25827b67381cfd188c9cbd82
    Signed-off-by: Jindong Yue <jindong.yue@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/30892
    Reviewed-by: Ji Luo <ji.luo@nxp.com>

--- imx9/evk_95/AndroidUboot.sh
+++ imx9/evk_95/AndroidUboot.sh
@@ -49,6 +49,8 @@ build_pre_image()
 	make -C ${BOARD_SM_PATH} SM_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" all config=mx95evkrpmsg 1>/dev/null || exit 1
 	make -C ${BOARD_SM_PATH} SM_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" cfg config=mx95evk-android 1>/dev/null || exit 1
 	make -C ${BOARD_SM_PATH} SM_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" all config=mx95evk-android 1>/dev/null || exit 1
+	make -C ${BOARD_SM_PATH} SM_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" cfg config=mx95evksof 1>/dev/null || exit 1
+	make -C ${BOARD_SM_PATH} SM_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" all config=mx95evksof 1>/dev/null || exit 1
 	echo Building imx-oei ...
 	make -C ${BOARD_OEI_PATH} really-clean
 	make -C ${BOARD_OEI_PATH} OEI_CROSS_COMPILE="${SM_OEI_CROSS_COMPILE}" board=mx95lp5 r=b0 oei=ddr d=1 all 1>/dev/null || exit 1
@@ -70,6 +72,8 @@ build_imx_uboot()
 		cp ${BOARD_SM_PATH}/build/mx95evk-android/m33_image.bin ${BOARD_MKIMAGE_PATH}/m33_image.bin
 	elif [ `echo $2 | cut -d '-' -f2` = "rpmsg" ]; then
 		cp ${BOARD_SM_PATH}/build/mx95evkrpmsg/m33_image.bin ${BOARD_MKIMAGE_PATH}/m33_image.bin
+	elif [ `echo $2 | cut -d '-' -f2` = "sof" ]; then
+		cp ${BOARD_SM_PATH}/build/mx95evksof/m33_image.bin ${BOARD_MKIMAGE_PATH}/m33_image.bin
 	else
 		cp ${BOARD_SM_PATH}/build/mx95evk/m33_image.bin ${BOARD_MKIMAGE_PATH}/m33_image.bin
 	fi
@@ -118,6 +122,8 @@ build_imx_uboot()
 	if echo "$2" | grep -q "15x15" ; then
 		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=${MKIMAGE_SOC} flash_all REV=B0 LPDDR_TYPE=lpddr4x OEI=YES || exit 1
 	elif [ `echo $2 | cut -d '-' -f2` = "rpmsg" ]; then
+		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=${MKIMAGE_SOC} flash_a55 REV=B0 LPDDR_TYPE=lpddr5 OEI=YES || exit 1
+	elif [ `echo $2 | cut -d '-' -f2` = "sof" ]; then
 		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=${MKIMAGE_SOC} flash_a55 REV=B0 LPDDR_TYPE=lpddr5 OEI=YES || exit 1
 	else
 		make -C ${IMX_MKIMAGE_PATH}/imx-mkimage/ SOC=${MKIMAGE_SOC} flash_all REV=B0 LPDDR_TYPE=lpddr5 OEI=YES || exit 1
