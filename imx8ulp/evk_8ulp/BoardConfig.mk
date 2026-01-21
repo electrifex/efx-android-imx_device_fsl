0255b7c63b3faa544b206e4c6fbd4d0bd51a3db7
Bo Zhang <bo.zhang@nxp.com>
2026-01-21 10:49:16 +0900

    MA-21258-5 Enable evk_8ulp low power display with default panel
    Use the default mipi-panel to demo low power display feature.
    
    Change-Id: I9305c7537b07cbcfd40f5a54adcc376c5284f9b7
    Signed-off-by: Bo Zhang <bo.zhang@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/30899
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- imx8ulp/evk_8ulp/BoardConfig.mk
+++ imx8ulp/evk_8ulp/BoardConfig.mk
@@ -155,7 +155,7 @@ ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
     TARGET_BOARD_DTS_CONFIG += imx8ulp-sof:imx8ulp-evk-sof-btsco.dtb
     # Support lpa
     TARGET_BOARD_DTS_CONFIG += imx8ulp-lpa:imx8ulp-evk-lpa.dtb
-    #TARGET_BOARD_DTS_CONFIG += imx8ulp-lpd:imx8ulp-evk-g1120b0mipi.dtb
+    TARGET_BOARD_DTS_CONFIG += imx8ulp-lpd:imx8ulp-evk-lpd.dtb
   endif
 else
   ifeq ($(IMX_NO_PRODUCT_PARTITION),true)
