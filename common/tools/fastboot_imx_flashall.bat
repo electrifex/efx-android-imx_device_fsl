5484bedcaf94070b38582aefecb14078143b1509
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2026-02-05 11:22:11 +0900

    MA-24493 delete imx8mp-evk-pciewifi.dtd and enable pcie in imx8mp-evk.dtd
    Both PCIe and SDIO WiFi is supported in imx8mp-evk.dtb. So delete the imx8mp-evk-pciewifi.dtb.
    
    Change-Id: I1b6f42c7872be3f628f58ba53fe533c195c8e5a5
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>
    Reviewed-on: http://androidsource.nxp.com/project/31051
    Reviewed-by: Yunjie Jia <yunjie.jia@nxp.com>
    (cherry picked from commit 8c782cd8bc20cd5210656a1079e01c6e8870f9f9)
    Reviewed-on: http://androidsource.nxp.com/project/31134
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>
    Reviewed-by: Elven Wang <elven.wang@nxp.com>

--- common/tools/fastboot_imx_flashall.bat
+++ common/tools/fastboot_imx_flashall.bat
@@ -83,7 +83,7 @@ set imx7ulp_uboot_feature=evk-uuu
 
 set imx8mm_dtb_feature=ddr4 m4 mipi-panel mipi-panel-rm67191
 set imx8mn_dtb_feature=mipi-panel mipi-panel-rm67191 rpmsg ddr4 ddr4-mipi-panel ddr4-mipi-panel-rm67191 ddr4-rpmsg
-set imx8mp_dtb_feature=rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg basler-ov5640 ov5640 dual-basler os08a20-ov5640 os08a20 sof revb4 rpmsg-revb4 lvds-panel-revb4 lvds-revb4 mipi-panel-revb4 mipi-panel-rm67191-revb4 basler-revb4 powersave-revb4 powersave-non-rpmsg-revb4 basler-ov5640-revb4 ov5640-revb4 sof-revb4 dual-basler-revb4 os08a20-ov5640-revb4 os08a20-revb4 frdm pciewifi
+set imx8mp_dtb_feature=rpmsg lvds-panel lvds mipi-panel mipi-panel-rm67191 basler powersave powersave-non-rpmsg basler-ov5640 ov5640 dual-basler os08a20-ov5640 os08a20 sof revb4 rpmsg-revb4 lvds-panel-revb4 lvds-revb4 mipi-panel-revb4 mipi-panel-rm67191-revb4 basler-revb4 powersave-revb4 powersave-non-rpmsg-revb4 basler-ov5640-revb4 ov5640-revb4 sof-revb4 dual-basler-revb4 os08a20-ov5640-revb4 os08a20-revb4 frdm
 set imx8mq_dtb_feature=wevk dual mipi-panel mipi-panel-rm67191 mipi
 set imx8qxp_dtb_feature=sof mipi-panel mipi-panel-rm67191 lvds0-panel ov5640-csi ov5640-parallel
 set imx8qm_dtb_feature=hdmi hdmi-rx mipi-panel mipi-panel-rm67191 md xen sof lvds1-panel revd mipi-panel-revd mipi-panel-rm67191-revd hdmi-revd hdmi-rx-revd md-revd lvds1-panel-revd sof-revd ov5640-csi0 ov5640-csi1 ov5640-csi0-revd ov5640-csi1-revd
@@ -318,7 +318,7 @@ echo                           ©¦   imx8mp       ©¦  rpmsg lvds-panel lvds mipi-
 echo                           ©¦                ©¦  basler-ov5640 ov5640 dual-basler os08a20-ov5640 os08a20 sof revb4 rpmsg-revb4 lvds-panel-revb4      ©¦
 echo                           ©¦                ©¦  mipi-panel-revb4 mipi-panel-rm67191-revb4 basler-revb4 powersave-revb4 powersave-non-rpmsg-revb4    ©¦
 echo                           ©¦                ©¦  basler-ov5640-revb4 ov5640.img-revb4 sof-revb4 dual-basler-revb4 os08a20-ov5640-revb4 lvds-revb4    ©¦
-echo                           ©¦                ©¦  os08a20-revb4 frdm  pciewifi                                                                        ©¦
+echo                           ©¦                ©¦  os08a20-revb4 frdm                                                                                  ©¦
 echo                           ©À©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©à©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©È
 echo                           ©¦   imx8mq       ©¦  wevk dual mipi-panel mipi-panel-rm67191 mipi                                                        ©¦
 echo                           ©À©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©à©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©¤©È
